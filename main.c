5ed1a39f22291f88892023a5582788a93c288382
Aayush Bhattarai <baayush061@gmail.com>
2026-05-14 00:35:32 +0200

    update2

--- main.c
+++ main.c
@@ -1,26 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <process.h>
 
 int main()
 {
     char input[1024];
+    char *list[100];
+    const char *clist[100];
+
     while (1)
     {
         printf("$: ");                      // constant input symbol
         fgets(input, sizeof(input), stdin); // ask for input using fgets to get long character
 
-        if (strcmp(input, "exit\n") == 0) // check if input is exit, if so exit
+        int len = strlen(input);               // when running without this we get a problem on running the string delimiter where at the end we got \n this trims the last \n
+        if (len > 0 && input[len - 1] == '\n') // if the length of our input is more than 0 meaning something is there, AND if 2nd last thing is \n that we get when pressing enter (last is alwyas \0)
+        {
+            input[len - 1] = '\0'; // basically remove the \n and replace that with \0
+        }
+
+        if (strcmp(input, "exit") == 0) // check if input is exit, if so exit
         {
             break;
         }
 
-        char *token = strtok(input, " ");
+        char *token = strtok(input, " "); // this is used to point where in my input there is space and return 0 if there is a space
 
-        while (token != NULL)
+        int i = 0;
+        while (token != NULL) // check till no space and 0 is not returned anymore
         {
-            printf("%s \n", token);
-            token = strtok(NULL, " ");
+            // printf("%s \n", token);    // printing to see input chopped in different lines
+            list[i] = token;
+            clist[i] = token;
+            i++;
+            token = strtok(NULL, " "); // pointing to next space and returning value if there is one
+        }
+        list[i] = NULL;
+        clist[i] = NULL;
+
+        if (list[0] != 0)
+        {
+            _spawnvp(_P_WAIT, clist[0], (const char *const *)clist);
         }
     }
 }
